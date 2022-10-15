3ac97cb2aaae749fae09405c98ebc33bb96f5714
Yehia <yehia71691@gmail.com>
2022-10-16 00:10:22 +0200

    Upload Changes

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY:String = "61afcece37114f38b9eec25ed733fb2c"
+        const val BASE_URL:String="https://newsapi.org/"
+    }
+}
